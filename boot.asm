abb6fff6f2c1bea19d4b9ef98d13ae1cd6c8671e
irohacoding <info@irohacoding.com>
2023-03-13 15:30:22 +0900

    Change indent character from space to tab and add comment

--- boot.asm
+++ boot.asm
@@ -2,26 +2,30 @@
 ; boot.asm
 ; nasm boot.asm -f bin -o boot.bin
 [bits 16]
-        mov ax, 0x07c0
-        mov ds, ax
+	;; mov: data transfer instruction
+	;; ax: register
+	;; 0x07c0: memory location
+	mov ax, 0x07c0
+	mov ds, ax
 
-        mov si, msg
-        cld
+	mov si, msg
+	cld
+
 ch_loop:
-        lodsb
-        or al, al
-        jz hang
-        mov ah, 0x0E
-        mov bh, 0
-        int 0x10
-        jmp ch_loop
+	lodsb
+	or al, al
+	jz hang
+	mov ah, 0x0E
+	mov bh, 0
+	int 0x10
+	jmp ch_loop
 
 hang:
-        jmp hang
+	jmp hang
 
-msg     db 'hello, this is itomos!', 13, 10, 0
+msg	db 'hello, this is itomos!', 13, 10, 0
 
-        times 510-($-$$) db 0
-        db 0x55
-        db 0xAA
+	times 510-($-$$) db 0
+	db 0x55
+	db 0xAA
 ;==========================================

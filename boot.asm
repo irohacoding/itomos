2fd4d2b74b02e4a8789949c819cc51c4a500ff04
irohacoding <info@irohacoding.com>
2023-03-15 13:19:20 +0900

    Add comments for si, cld and more

--- boot.asm
+++ boot.asm
@@ -2,19 +2,25 @@
 ; boot.asm
 ;-----------------------------------------------
 [bits 16]
-	; mov: data transfer instruction
-	; ax: general-purpose register (GPR)
-	; 0x07c0: memory location
+	; [mov] data transfer instruction
+	; [ax] accumulator (general-purpose register)
+	; [0x07c0] memory location
 	mov ax, 0x07c0
-	; mov: data transfer instruction
-	; ds: 16-bit segment register
-	; ax: general-purpose register (GPR)
+	; [mov] data transfer instruction
+	; [ds] 16-bit segment register
+	; [ax] accumulator (general-purpose register)
 	mov ds, ax
 
+	; [mov] data transfer instruction
+	; [si] source index register
+	; [msg] label
 	mov si, msg
+	; [cld] clear direction flag
 	cld
 
+; [ch_loop:] label
 ch_loop:
+	; [lodsb] load byte at address ds:si into al
 	lodsb
 	or al, al
 	jz hang
@@ -23,11 +29,17 @@ ch_loop:
 	int 0x10
 	jmp ch_loop
 
+; [hang:] label
 hang:
 	jmp hang
 
+; [msg] label
 msg	db 'hello, this is itomos!', 13, 10, 0
 
+	; [-] ordinary subtraction
+	; [$] the assembly position at the begining
+	;     of the line containing the expression
+	; [$$] the begining of the current section
 	times 510-($-$$) db 0
 	db 0x55
 	db 0xAA

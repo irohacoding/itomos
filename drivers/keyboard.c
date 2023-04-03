5f189ae7fe364c9941f25be2f0d8f5bde3f430a6
irohacoding <info@irohacoding.com>
2023-04-03 14:32:43 +0900

    Change behavior when pressing shift key
    Like a general keyboard, hold down shift key and press another alphabetic key to capitalize, and symbols such as @ sign can also be input.

--- drivers/keyboard.c
+++ drivers/keyboard.c
@@ -1,15 +1,20 @@
 /* keyboard.c */
 #include "keyboard.h"
+#include "screen.h"
 #include "ports.h"
 #include "../cpu/isr.h"
-#include "screen.h"
 #include "../kernel/kernel.h"
 #include "../kernel/util.h"
 
 #define BACKSPACE 0x0E
 #define ENTER 0x1C
+#define LSHIFT 0x2A
+#define RSHIFT 0x36
 
 static char key_buffer[256];
+
+/* 0: released 1: pressed */
+int shift_status;
 
 #define SC_MAX 57
 
@@ -17,19 +22,28 @@ const char *sc_name[] = { "ERROR", "ESC", "1", "2", "3", "4",
   "5", "6", "7", "8", "9", "0", "-", "=", "Backspace", "Tab",
   "Q", "W", "E", "R", "T", "Y", "U", "I", "O", "P", "[", "]",
   "Enter", "LCtrl", "A", "S", "D", "F", "G", "H", "J", "K",
-  "L", ";", "'", "`", "LShift", "\\", "Z", "X", "C", "V",
-  "B", "N", "M", ",", ".", "/", "RShift", "Keypad *", "LAlt", "SPC" };
+  "L", ";", "'", "`", "LShift", "\\", "Z", "X", "C", "V", "B",
+  "N", "M", ",", ".", "/", "RShift", "Keypad *", "LAlt", "SPC" };
 
 const char sc_ascii[] = { '?', '?', '1', '2', '3', '4', '5', '6',
-  '7', '8', '9', '0', '-', '=', '?', '?', 'Q', 'W', 'E', 'R', 'T',
-  'Y', 'U', 'I', 'O', 'P', '[', ']', '?', '?', 'A', 'S', 'D', 'F',
-  'G', 'H', 'J', 'K', 'L', ';', '\'', '`', '?', '\\', 'Z', 'X', 'C',
-  'V', 'B', 'N', 'M', ',', '.', '/', '?', '?', '?', ' ' };
+  '7', '8', '9', '0', '-', '=', '?', '?', 'q', 'w', 'e', 'r', 't',
+  'y', 'u', 'i', 'o', 'p', '[', ']', '?', '?', 'a', 's', 'd', 'f',
+  'g', 'h', 'j', 'k', 'l', ';', '\'', '`', '?', '\\', 'z', 'x',
+  'c', 'v', 'b', 'n', 'm', ',', '.', '/', '?', '?', '?', ' ' };
+
+const char sc_shift_ascii[] = { '?', '?', '!', '@', '#', '$', '%',
+  '^', '&', '*', '(', ')', '_', '+', '?', '?', 'Q', 'W', 'E', 'R',
+  'T', 'Y', 'U', 'I', 'O', 'P', '{', '}', '?', '?', 'A', 'S', 'D',
+  'F', 'G', 'H', 'J', 'K', 'L', ':', '"', '~', '?', '|', 'Z', 'X',
+  'C', 'V', 'B', 'N', 'M', '<', '>', '?', '?', '?', '?', ' ' };
 
 static void keyboard_callback(registers_t *regs) {
   uint8_t scancode = port_byte_in(0x60);
 
-  if (scancode > SC_MAX) return;
+  if (scancode > SC_MAX) {
+    if (scancode - 0x80 == LSHIFT || scancode - 0x80 == RSHIFT) shift_status = 0;
+    return;
+  }
 
   if (scancode == BACKSPACE) {
     backspace(key_buffer);
@@ -38,8 +52,15 @@ static void keyboard_callback(registers_t *regs) {
     kprint("\n");
     user_input(key_buffer);
     key_buffer[0] = '\0';
+  } else if (scancode == LSHIFT || scancode == RSHIFT) {
+    shift_status = 1;
   } else {
-    char letter = sc_ascii[(int) scancode];
+    char letter;
+    if (shift_status) {
+      letter = sc_shift_ascii[(int) scancode];
+    } else {
+      letter = sc_ascii[(int) scancode];
+    }
     append(key_buffer, letter);
     char str[2] = {letter, '\0'};
     kprint(str);
